2efa1a31bfaf45bbb28e5f60cc3864f36b59a735
manimalladi <manimalladi05@gmail.com>
2025-07-11 10:30:16 +0530

    changes

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css
     ">
-<link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="index.css">
 
     <style>
 
     </style>
 </head>
+
 <body>
-    
-    <img src="img3.png" alt="" class="img-fluid img_section" >
+
+    <img src="img3.png" alt="" class="img-fluid img_section">
 </body>
+
 </html>
